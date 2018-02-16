80b4ddde69792ab94db31152a4b249ef48b83312
motolola <motolola@icloud.com>
2018-02-16 01:23:24 +0000

    init with limit services

---
+++ limits-service/src/main/java/com/motolola/microservices/limitsservice/LimitsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.motolola.microservices.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServiceApplication {
+
+	public static void main(String[] args) {
+		//Just to test ...
+		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+}
