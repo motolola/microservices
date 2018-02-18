b83838c0d0bd5d427595c177b9a8b3b3473cffc6
motolola <motolola@icloud.com>
2018-02-18 21:46:22 +0000

    Setting up dynamic port ….

--- currency-exchange-service/src/main/java/com/motolola/microservices/currencyexchangeservice/bean/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/motolola/microservices/currencyexchangeservice/bean/ExchangeValue.java
@@ -8,6 +8,7 @@ public class ExchangeValue {
 	private String from;
 	private String to;
 	private BigDecimal conversionMultiple;
+	private int port;
 	
 	public ExchangeValue() {
 		
@@ -21,6 +22,13 @@ public class ExchangeValue {
 	}
 	public Long getId() {
 		return id;
+	}
+	
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
 	}
 	public void setId(Long id) {
 		this.id = id;
--- currency-exchange-service/src/main/java/com/motolola/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/motolola/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -2,6 +2,9 @@ package com.motolola.microservices.currencyexchangeservice.controllers;
 
 import java.math.BigDecimal;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +14,17 @@ import com.motolola.microservices.currencyexchangeservice.bean.ExchangeValue;
 @RestController
 public class CurrencyExchangeController {
 	
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchaneValue(@PathVariable String from, @PathVariable String to) {
 		
-		return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		
+		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		
+		return exchangeValue;
 		
 	}
 
