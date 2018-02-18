9d2bcef81afdc50ff120be77ca42e1fac7e4b3ee
motolola <motolola@icloud.com>
2018-02-18 17:02:29 +0000

    Getting values from limits service property file.

--- limits-service/src/main/java/com/motolola/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/motolola/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,16 +1,21 @@
 package com.motolola.microservices.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.motolola.microservices.limitsservice.bean.LimitConfiguration;
+import com.motolola.microservices.limitsservice.config.Configuration;
 
 @RestController
 public class LimitsConfigurationController {
 	
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
-		return new LimitConfiguration(1000, 1);
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 		
 	}
 
